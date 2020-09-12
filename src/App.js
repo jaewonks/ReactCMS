1f400b04c8bed2bad4772e0e31a6f5e795f9fed8
Jaewon Kim <jaewon@Jaewons-MBP.cust.communityfibre.co.uk>
2020-09-12 23:15:32 +0100

    2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const customers = [{
+    'id':1,
+    'image':'http://placeimg.com/64/64/any',
+    'name':'홍길동',
+    'birthday':'961222',
+    'gender':'남자',
+    'job':'대학생'
+}, {
+  'id':2,
+  'image':'http://placeimg.com/64/64/any',
+  'name':'나동빈',
+  'birthday':'961222',
+  'gender':'남자',
+  'job':'대학생'
+}, {
+  'id':3,
+  'image':'http://placeimg.com/64/64/any',
+  'name':'이순신',
+  'birthday':'961222',
+  'gender':'남자',
+  'job':'대학생'
+}]
+
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        {customers.map(c => {
+            return(
+              <Customer
+                key = {c.id} 
+                id = {c.id}  
+                image = {c.image}  
+                name = {c.name}  
+                birthday = {c.birthday}  
+                gender = {c.gender}  
+                job = {c.job}  
+                />
+             );
+           })
+          }
+        </div>
+    );
+  }
 }
 
 export default App;
